99a7b2e3fd0957b1ac1ed3e08a915c286a1334a0
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-11-12 12:08:31 -0700

    Rename statistics module

--- esmlab/accessors.py
+++ esmlab/accessors.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 import xarray as xr
-from esmlab.dataarray_statistics import (
+from esmlab.statistics import (
     da_weighted_mean,
     da_weighted_sum,
     da_weighted_std,
