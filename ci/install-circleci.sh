6eeced7b10a72d4d1a10539ec3e2f49d4bf03473
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-12-19 11:09:51 -0700

    move test commands from config file to script

--- ci/install-circleci.sh
+++ ci/install-circleci.sh
@@ -16,4 +16,24 @@ pip install --no-deps -e .
 conda list
 echo "[finished install]"
 
+echo
+echo "[Running Tests]"
+py.test --junitxml=test-reports/junit.xml --cov=./
+
+
+echo
+echo "[Upload coverage]"
+# Upload coverage in each build, codecov.io merges the reports
+codecov 
+
+
+# Check documentation build only in one job
+if [ "${PYTHON_VERSION}" = "3.6" ]; then
+  echo 
+  echo "[Building documentation]"
+  pushd docs
+  make html
+  popd
+fi
+
 exit 0
