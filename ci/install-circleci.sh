8997fa682d0470745eb42e0e08c5cc7f686ce578
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-12-19 10:32:12 -0700

    Add python 2.7 and python 3.6 jobs to CI

--- ci/install-circleci.sh
+++ ci/install-circleci.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 set -e
+set -eo pipefail
 
+export PYTHON_VERSION=$1
 echo
 echo "[install dependencies]"
+conda create -y -q -n esmlab-dev python=${PYTHON_VERSION}
 conda env update -f environment-dev.yml
 source activate esmlab-dev
-conda list
 
 echo
 echo "[install esmlab]"
 pip install --no-deps -e .
-
+conda list
 echo "[finished install]"
 
 exit 0
