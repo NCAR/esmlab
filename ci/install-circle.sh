73bb6496aa966d9aaf6560612a1a326de4397a6e
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-12-21 13:44:50 -0700

    Add g++ and gcc

--- ci/install-circle.sh
+++ ci/install-circle.sh
@@ -2,6 +2,8 @@
 
 set -e
 set -eo pipefail
+
+apt-get update; apt-get install -y gcc g++
 conda config --set always_yes true --set changeps1 false --set quiet true
 conda update -q conda
 conda install conda-build anaconda-client --yes --quiet
