b4399685cbefdd5077a7c5483ad641d1ae74197d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-01-04 12:47:08 -0700

    Add manifest and requirements

--- setup.py
+++ setup.py
@@ -5,31 +5,26 @@
 
 from setuptools import setup, find_packages
 import versioneer
-import io
 from os.path import exists
 
-readme = io.open("README.rst").read() if exists("README.rst") else " "
+if exists("README.rst"):
+    with open("README.rst") as f:
+        long_description = f.read()
+else:
+    long_description = ""
 
-
-requirements = ["pandas>=0.23.0", "dask", "xarray", "netcdf4", "cftime"]
+with open("requirements.txt") as f:
+    install_requires = f.read().strip().split("\n")
 
 test_requirements = ["pytest"]
 
 setup(
     maintainer="Anderson Banihirwe",
     maintainer_email="abanihi@ucar.edu",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache-2.0",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
-    ],
-    description=readme,
-    install_requires=requirements,
+    description="Tools for working with earth system multi-model analyses with xarray",
+    install_requires=install_requires,
     license="Apache License 2.0",
-    long_description=readme,
+    long_description=long_description,
     keywords="esmlab xarray cmip",
     name="esmlab",
     packages=find_packages(),
