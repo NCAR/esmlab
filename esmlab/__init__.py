d34e869fc21825da2b77323eff6ff9c76e51b459
mclong <mclong@ucar.edu>
2019-02-09 12:30:59 -0700

    added regrid!

--- esmlab/__init__.py
+++ esmlab/__init__.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
+from .config import set_options, get_options
+
 """Top-level package for esmlab."""
 from ._version import get_versions
 from . import climatology
 from . import statistics
 
-_module_imports = (climatology, statistics)
 __version__ = get_versions()["version"]
 del get_versions
 
+
+_module_imports = (climatology, statistics)
 __all__ = list(map(lambda x: x.__name__, _module_imports))
---
+++ esmlab/config.py
@@ -0,0 +1,97 @@
+""" The configuration script: set global settings.
+"""
+
+from __future__ import absolute_import, print_function
+
+import os
+
+import yaml
+
+GRIDFILE_DIRECTORY = "gridfile_directory"
+GRID_DEFITIONS_FILE = "grid_defitions_file"
+
+_here = os.path.abspath(os.path.dirname(__file__))
+_config_dir = os.path.join(os.path.expanduser("~"), ".esmlab")
+_path_config_yml = os.path.join(_config_dir, "config.yml")
+
+if os.path.exists(".config-esmlab.yml"):
+    _path_config_yml = os.path.join(".config-esmlab.yml")
+
+SETTINGS = {
+    GRIDFILE_DIRECTORY: os.path.join(_config_dir, "esmlab-grid-files"),
+    GRID_DEFITIONS_FILE: os.path.join(_here, "grid_definitions.yml"),
+}
+
+for key in [GRIDFILE_DIRECTORY]:
+    os.makedirs(SETTINGS[key], exist_ok=True)
+
+
+def _check_path_write_access(value):
+    value = os.path.abspath(os.path.expanduser(value))
+    if os.path.exists(value):
+        if not os.access(value, os.W_OK):
+            print(f"no write access to: {value}")
+            return False
+        return True
+
+    try:
+        os.makedirs(value)
+        return True
+    except (OSError, PermissionError) as err:
+        print(f"could not make directory: {value}")
+        raise err
+
+
+def _full_path(value):
+    return os.path.abspath(os.path.expanduser(value))
+
+
+def _check_exists(value):
+    return os.path.exists(value)
+
+
+_VALIDATORS = {
+    GRIDFILE_DIRECTORY: _check_path_write_access,
+    GRID_DEFITIONS_FILE: _check_exists,
+}
+
+_SETTERS = {GRIDFILE_DIRECTORY: _full_path}
+
+
+class set_options(object):
+    """Set configurable settings."""
+
+    def __init__(self, **kwargs):
+        self.old = {}
+        for key, val in kwargs.items():
+            if key not in SETTINGS:
+                raise ValueError(
+                    f"{key} is not in the set of valid settings:\n {set(SETTINGS)}"
+                )
+            if key in _VALIDATORS and not _VALIDATORS[key](val):
+                raise ValueError(f"{val} is not a valid value for {key}")
+            self.old[key] = SETTINGS[key]
+        self._apply_update(kwargs)
+
+    def _apply_update(self, settings_dict):
+        for key, val in settings_dict.items():
+            if key in _SETTERS:
+                settings_dict[key] = _SETTERS[key](val)
+        SETTINGS.update(settings_dict)
+
+    def __enter__(self):
+        return
+
+    def __exit__(self, type, value, traceback):
+        self._apply_update(self.old)
+
+
+def get_options():
+    return SETTINGS
+
+
+if os.path.exists(_path_config_yml):
+    with open(_path_config_yml) as f:
+        dot_file_settings = yaml.load(f)
+    if dot_file_settings:
+        set_options(**dot_file_settings)
