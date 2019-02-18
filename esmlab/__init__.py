52659491d469f60dff19977df93dabe6242f4fef
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-02-18 12:29:08 -0700

    Add datasets module to __init__.py

--- esmlab/__init__.py
+++ esmlab/__init__.py
@@ -7,6 +7,7 @@ from .config import set_options, get_options
 from ._version import get_versions
 from . import climatology
 from . import statistics
+from . import datasets
 
 if sys.version_info > (3, 0):
     from .regrid import regridder
@@ -15,5 +16,5 @@ __version__ = get_versions()["version"]
 del get_versions
 
 
-_module_imports = (climatology, statistics)
+_module_imports = (climatology, statistics, datasets, set_options, get_options)
 __all__ = list(map(lambda x: x.__name__, _module_imports))
