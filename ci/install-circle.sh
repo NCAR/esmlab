1b59e59b1d29bf38c73b4447a661468d2b245ea6
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-03-03 17:17:59 -0700

    Update CircleCI configuration

--- ci/install-circle.sh
+++ ci/install-circle.sh
@@ -3,9 +3,7 @@
 set -e
 set -eo pipefail
 
-apt-get update; apt-get install -y make
 conda config --set always_yes true --set changeps1 false --set quiet true
-conda update -q conda
 conda config --add channels conda-forge
 conda env create -f ci/environment-dev-${PYTHON}.yml --name=${ENV_NAME} --quiet
 conda env list
