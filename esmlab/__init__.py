262e586ac1759261ef50192bcb5538fcbc0a895f
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-03-28 19:38:51 -0600

    Fix formatting issues

--- esmlab/__init__.py
+++ esmlab/__init__.py
@@ -5,6 +5,9 @@
 import sys
 
 from . import datasets
+from ._version import get_versions
+from .core import EsmlabAccessor, mon_anomaly, mon_climatology, ann_mean
+from .common_utils import esmlab_xr_set_options
 from .statistics import (
     weighted_corr,
     weighted_mean,
@@ -13,9 +16,6 @@ from .statistics import (
     weighted_cov,
     weighted_sum,
 )
-from .core import EsmlabAccessor, mon_anomaly, mon_climatology, ann_mean
-from ._version import get_versions
-from .common_utils import esmlab_xr_set_options
 
 __version__ = get_versions()['version']
 del get_versions
