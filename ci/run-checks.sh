aec77fcc745419f60bc255e9e7d66af7229a5ce7
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-04-03 23:45:10 -0600

    Run tests with one process

--- ci/run-checks.sh
+++ ci/run-checks.sh
@@ -4,6 +4,6 @@ set -e
 set -eo pipefail
 
 source activate ${ENV_NAME}
-pytest -n 2 --junitxml=test-reports/junit.xml --cov=./ --verbose
+pytest -n 1 --junitxml=test-reports/junit.xml --cov=./ --verbose
 echo "[Upload coverage]"
 codecov
