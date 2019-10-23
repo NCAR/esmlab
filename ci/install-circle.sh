e08be8ca1fe7c11c600e64d83ffa9c3d49a90e78
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-10-23 08:30:49 -0600

    conda/miniconda3 --> continuumio/miniconda3

--- ci/install-circle.sh
+++ ci/install-circle.sh
@@ -7,7 +7,6 @@ conda config --set always_yes true --set changeps1 false --set quiet true
 conda config --add channels conda-forge
 conda env create -f ci/environment-dev-${PYTHON}.yml --name=${ENV_NAME} --quiet
 conda env list
-source activate ${ENV_NAME}
-pip install pip --upgrade
+conda activate ${ENV_NAME}
 pip install --no-deps --quiet -e .
 conda list -n ${ENV_NAME}
